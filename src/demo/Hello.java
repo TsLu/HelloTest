8e79e65dc32f08a079e8b9b854e363a1f9379337
TsLu <601501823@qq.com>
2015-11-25 14:44:28 +0800

    Initial commit

---
+++ src/demo/Hello.java
@@ -0,0 +1,11 @@
+package demo;
+
+/**
+ * Created by tingshu on 2015/11/25.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("git测试2");
+        System.out.println("Hello git");
+    }
+}
